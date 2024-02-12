5a5fee23b22accb0c8c20b9c8ec578b417985648
fokus1304 <114816579+fokus1304@users.noreply.github.com>
2024-02-12 15:10:24 +0300

    Add files via upload

---
+++ zadanie 5/sketch.ino
@@ -0,0 +1,16 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  delay(3000);
+  digitalWrite(13, LOW);
+  delay(900);
+}
+ 
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(900);
+}
